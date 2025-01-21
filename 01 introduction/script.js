33058610e34dd30c244c8c099ba50d21d2ad3ccc
Abhishekkanu <abhishekkanu1922@gmail.com>
2025-01-22 01:01:39 +0530

    files added

---
+++ 01 introduction/script.js
@@ -0,0 +1,38 @@
+function sayHello(){alert("hello world")}
+document.write('This is my fisrt javascript page');
+document.write('<h1>This is my fisrt javascript page</h1>');
+
+var pi=3.14;
+var person="are you GOD";
+var answer='YES I AM!';
+document.write(pi+"<br>");
+document.write(person+"<br>");
+document.write(answer+"<br>");
+
+
+function myfunction()
+{
+    var carname="welcome";
+    document.getElementById("demo").innerHTML=carname;
+}
+
+
+var num=10;
+console.log(num);
+console.log(typeof(num));
+
+num=false;
+console.log(num);
+console.log(typeof(num));
+
+num="ABHISHEK";
+console.log(num);
+console.log(typeof(num));
+
+num=null;
+console.log(num);
+console.log(typeof(num));
+
+num=undefined;
+console.log(num);
+console.log(typeof(num));
